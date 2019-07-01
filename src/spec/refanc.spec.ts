ec5f7c56a574e73afc3598b96a44b39c5221275a
Manabu Niseki <manabu.niseki@gmail.com>
2019-07-02 08:31:58 +0900

    refactor: simplify specs

--- src/spec/refanc.spec.ts
+++ src/spec/refanc.spec.ts
@@ -1,23 +1,11 @@
 import { refang } from "../index";
 
-describe("refang", () => {
-  test("example[.]com", () => {
-    expect(refang("example[.]com")).toBe("example.com");
-  });
+test("refang", () => {
+  expect(refang("example[.]com")).toBe("example.com");
+  expect(refang("test[.]example[.]com")).toBe("test.example.com");
+  expect(refang("test@example[.]com")).toBe("test@example.com");
+  expect(refang("hxxp://example[.]com")).toBe("http://example.com");
+  expect(refang("https://example[.]com")).toBe("https://example.com");
 
-  test("test[.]example[.]com", () => {
-    expect(refang("test[.]example[.]com")).toBe("test.example.com");
-  });
-
-  test("test@example[.]com", () => {
-    expect(refang("test@example[.]com")).toBe("test@example.com");
-  });
-
-  test("hxxp://example[.]com", () => {
-    expect(refang("hxxp://example[.]com")).toBe("http://example.com");
-  });
-
-  test("hxxps://example[.]com", () => {
-    expect(refang("https://example[.]com")).toBe("https://example.com");
-  });
+  expect(refang("example.com")).toBe("example.com");
 });
