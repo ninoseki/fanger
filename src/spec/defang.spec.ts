58051e8fbc0159cf751c773983e2effac11f6d43
Manabu Niseki <manabu.niseki@gmail.com>
2019-07-02 20:43:33 +0900

    refactor: renew defang methods

--- src/spec/defang.spec.ts
+++ src/spec/defang.spec.ts
@@ -1,11 +1,20 @@
 import { defang } from "../index";
 
 test("defang", () => {
+  expect(defang("1.1.1.1")).toBe("1[.]1.1.1");
+
   expect(defang("example.com")).toBe("example[.]com");
-  expect(defang("test.example.com")).toBe("test[.]example[.]com");
+  expect(defang("test.example.com")).toBe("test.example[.]com");
   expect(defang("test@example.com")).toBe("test@example[.]com");
   expect(defang("http://example.com")).toBe("hxxp://example[.]com");
   expect(defang("https://example.com")).toBe("hxxps://example[.]com");
+
+  expect(defang("hoge.example.com")).toBe("hoge.example[.]com");
+  expect(defang("apple.com.example.com")).toBe("apple[.]com.example[.]com");
+  expect(defang("https://apple.com.example.com")).toBe(
+    "hxxps://apple[.]com.example[.]com"
+  );
+  expect(defang("http://1.1.1.1")).toBe("hxxp://1[.]1.1.1");
 
   expect(defang("example.none_tld")).toBe("example.none_tld");
 });
