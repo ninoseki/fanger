51571fa4485ca3b8225c1436cbf51716502f7c58
Manabu Niseki <manabu.niseki@gmail.com>
2021-01-14 20:25:07 +0900

    refactor: fix/disable ESLint errors

--- src/bin/defang.ts
+++ src/bin/defang.ts
@@ -3,6 +3,7 @@ import getStdin from "get-stdin";
 
 import { defang } from "../index";
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 getStdin().then((str) => {
   const defanged = defang(str);
   process.stdout.write(defanged);
--- src/bin/refang.ts
+++ src/bin/refang.ts
@@ -3,6 +3,7 @@ import getStdin from "get-stdin";
 
 import { refang } from "../index";
 
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 getStdin().then((str) => {
   const refanged = refang(str);
   process.stdout.write(refanged);
